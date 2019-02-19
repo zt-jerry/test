166056bd9add52c8688fa8c61f2c75d18de2e47d
itcast <bos@itcast.cn>
2019-02-19 19:56:46 +0800

    idea提交

---
+++ src/main/java/com/itheima/Application.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+/**
+ * @author Jerry
+ * @description:
+ * @date 2019/2/19 19:55
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Application");
+    }
+
+}
